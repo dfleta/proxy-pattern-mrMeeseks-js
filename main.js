349dcda9702451b0703b57cc7737144041f8baeb
David <gelpiorama@gmail.com>
2018-11-22 17:58:08 +0100

    inicio del proyecto MrMeeseeks

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function main() {
+
+    let reality = [];
+
+    // para practicar un singleton con closures
+    // el método es estatico
+    let box = Box.getInstanceBox();
+
+    box.pushButton(setMrMe);
+
+
+}
+
+main();
