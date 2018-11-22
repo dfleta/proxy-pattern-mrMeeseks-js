b647087d16483996efdee6be55d6cfbd3f9a7a37
David <gelpiorama@gmail.com>
2018-11-22 19:37:46 +0100

    createMrMeeseeks y pushButton, toString de MrMeeseeks

--- box.js
+++ box.js
@@ -1,11 +1,32 @@
 
 
-function Box() {
-    this.name = "Rick's box";
 
+function MrMeeseeks() {
+    this.id = "the one";
 }
 
+MrMeeseeks.prototype.toString = function() {
+    return this.id;
+}
 
+/**
+ * clase Box
+ */
+
+function Box() {
+    this.name = "Rick's box";
+}
+
+Box.prototype.createMrMeeseeks = function() {
+    let prototipo = new MrMeeseeks();
+    return Object.create(prototipo);
+}
+
+Box.prototype.pushButton = function(reality) {
+    reality.push(this.createMrMeeseeks());
+}
+
+// singleton
 function getBox() {
     
     let boxInstance = new Box();
--- testing.js
+++ testing.js
@@ -8,4 +8,11 @@ console.log(box.name);
 var box2 = importaBox.singletonBox().get();
 console.log(box2.name);
 
+var mrMeeseeks = box.createMrMeeseeks();
+console.log(mrMeeseeks.id);
+
+var reality = [];
+box.pushButton(reality);
+console.log(reality.toString());
+
 
